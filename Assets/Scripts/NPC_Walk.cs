c2bcd9be57e2ea0ffe18e46b90e26ed0e0c8da2a
Ryan87960 <zack87960@gmail.com>
2026-01-10 22:34:13 +0800

    add dying animation
    新增死亡動畫

--- Assets/Scripts/NPC_Walk.cs
+++ Assets/Scripts/NPC_Walk.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 public class NPC_Walk : MonoBehaviour
@@ -6,25 +7,68 @@ public class NPC_Walk : MonoBehaviour
     private Animator 動畫器;
     public Transform 目標;
     public float 距離 = 0;
+
+    public TextMeshPro 血量文字;
+    public int 血量 = 100;
+    int 原始血量;
+    public Transform 血條;
+    bool 開始攻擊 = false;
+    float 攻擊間距 = 2f;
+    float 下次可攻擊時間;
+    public float 攻擊距離 = 1.2f;
+
     void Start()
     {
         導航 = GetComponent<NavMeshAgent>();
         動畫器 = GetComponent<Animator>();
+        原始血量 = 血量;
+        血量文字.text = 血量.ToString();
+        導航.stoppingDistance = 攻擊距離;
     }
     void Update()
     {
         if (目標 != null)
         {
-            
+            導航.SetDestination(目標.position);
             距離 = Vector3.Distance(目標.position, this.transform.position);
-            if (距離 >= 3.1f)
+            if (距離 <= 攻擊距離)
             {
                 動畫器.SetBool("isWalk", true);
-                導航.SetDestination(目標.position);
+                開始攻擊 = true;
             }
             else 
             {
-                動畫器.SetBool("isWalk", false); 
+                動畫器.SetBool("isWalk", false);
+                開始攻擊 = false;
+            }
+            if(開始攻擊)
+            { 
+                if(血量 <= 0) return;
+                if (Time.time >= 下次可攻擊時間)
+                {
+                    動畫器.SetTrigger("isAttack");
+                    下次可攻擊時間 = Time.time + 攻擊間距;
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "子彈")
+        {
+            血量--;
+            血量文字.text = 血量.ToString();
+            float 血條比例 = (float)血量 / (float)原始血量;
+            血條.localScale = new Vector3(血條比例, 1, 1);
+            if(血量 <= 0)
+            {
+                動畫器.SetTrigger("isDie");
+                Destroy(this.gameObject, 3f);
+
+            }
+            else
+            {
+                動畫器.SetTrigger("isHit");
             }
         }
     }
